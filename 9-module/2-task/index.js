3d96eaed64dd96ed41b05288b3c5cdc0c8b8b25b
IrinaKlevko <klevko8@gmail.com>
2023-12-01 12:55:17 +0300

    module 9 task 2

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -16,6 +16,80 @@ export default class Main {
   }
 
   async render() {
-    // ... ваш код
+    this.addCarouselComponent();
+    this.addRibbonMenuComponent();
+    this.addStepSliderComponent();
+    this.addCartIconComponent();
+    this.addCartComponent();
+
+    let response = await fetch('products.json');
+    this.products = await response.json();
+
+    this.renderProductsGrid(this.products);
+    this.filter();
+    this.addListeners();
   }
+
+  addCarouselComponent = () => {
+    this.carousel = new Carousel(slides);
+    document.querySelector('[data-carousel-holder]').append(this.carousel.elem);
+  };
+
+  addRibbonMenuComponent = () => {
+    this.ribbonMenu = new RibbonMenu(categories);
+    document.querySelector('[data-ribbon-holder]').append(this.ribbonMenu.elem);
+  };
+
+  addStepSliderComponent = () => {
+    this.stepSlider = new StepSlider({steps: 5, value: 3});
+    document.querySelector('[data-slider-holder]').append(this.stepSlider.elem);
+  };
+
+  addCartIconComponent = () => {
+    this.cartIcon = new CartIcon();
+    document.querySelector('[data-cart-icon-holder]').append(this.cartIcon.elem);
+  };
+
+  addCartComponent = () => {
+    this.cart = new Cart(this.cartIcon);
+  };
+
+  renderProductsGrid = (products) => {
+    this.productsGrid = new ProductsGrid(products);
+    document.querySelector('[data-products-grid-holder]').append(this.productsGrid.elem);
+  };
+
+  filter = () => {
+    this.productsGrid.updateFilter({
+      noNuts: document.getElementById('nuts-checkbox').checked,
+      vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
+      maxSpiciness: this.stepSlider.value,
+      category: this.ribbonMenu.value
+    });
+  };
+
+  addListeners = () => {
+    document.body.addEventListener('product-add', event => {
+      this.cart.addProduct(this.products.find(item => item.id === event.detail));
+    });
+
+    this.stepSlider.elem.addEventListener('slider-change', event => {
+      this.productsGrid.updateFilter({maxSpiciness: event.detail});
+    });
+
+    this.ribbonMenu.elem.addEventListener('ribbon-select', event => {
+      this.productsGrid.updateFilter({category: event.detail});
+    });
+
+    let filterNuts = document.querySelector('#nuts-checkbox');
+    let filterVegeterian = document.querySelector('#vegeterian-checkbox');
+
+    filterNuts.onchange = () => {
+      this.productsGrid.updateFilter({ noNuts: filterNuts.checked });
+    };
+
+    filterVegeterian.onchange = () => {
+      this.productsGrid.updateFilter({ vegeterianOnly: filterVegeterian.checked });
+    };
+  };
 }
